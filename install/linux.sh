e166d2df61867f5c67ad58a5ce8855579008017e
Andrew Gunsch <andrew@gunsch.cc>
2022-06-11 13:14:16 -0700

    Newer updates for Ubuntu upgrade, including:
    * node 10 / npm issues
    * signal desktop
    * git

--- install/linux.sh
+++ install/linux.sh
@@ -16,19 +16,54 @@ if ! command -v fd >/dev/null 2>&1; then
       rm fd_${FD_VERSION}_amd64.deb
 fi
 
+if [ ! -e /etc/apt/sources.list.d/google-chrome.list ]; then
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+  sudo apt update
+fi
+
 if [ ! -e /etc/apt/sources.list.d/github-cli.list ]; then
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+  wget -q https://cli.github.com/packages/githubcli-archive-keyring.gpg -O - | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
   sudo apt update
 fi
 
+if [ ! -e /etc/apt/sources.list.d/signal-xenial.list ]; then
+	wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+	cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+		sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+  sudo apt update
+fi
+
+
+# Ubuntu 20.04 installs node 10, but npm doesn't work with node 10 anymore.
+# Need to upgrade first, then use npm -> n -> node to get latest
+# This section can be removed if Ubuntu starts shipping newer node versions with default OS.
+node_version=$(node --version)
+if [ ${node_version:1:2} -lt 12 ]; then
+    ## Update NodeJS
+    curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
+
+# Update node via npm -> n
+sudo npm install -g n
+sudo n stable
+
 sudo apt-get install \
-   ack-grep \
-   gh \
-   jc \
+   ack \
+   chrome-gnome-shell \
+   curl \
+   git \
+   google-chrome-stable \
+   htop \
    jq \
    magic-wormhole \
    mosh \
    ncdu \
-   npm
+	 signal-desktop \
+   terminator \
+   vim
 
